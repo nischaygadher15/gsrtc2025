f95b3dc93b2b7d6c5c2b7d2711242ed84d40e512
nischaygadher15 <nischaygadher15@gmail.com>
2025-11-01 11:19:18 +0530

    Sleeper Seat component status section completed

--- src/components/SleeperBusComp/SleeperBus.tsx
+++ src/components/SleeperBusComp/SleeperBus.tsx
@@ -28,32 +28,58 @@ export const SleeperSeat = ({
   status: SeatStatus;
   text: string;
 }) => {
-  let [seatStatus, setSeatStatus] = useState<SeatStatus>(status);
+  const [seatStatus, setSeatStatus] = useState<SeatStatus>(status);
+  const isSeatBooked =
+    seatStatus === "booked" ||
+    seatStatus === "bookedByMale" ||
+    seatStatus === "bookedByFemale"
+      ? true
+      : false;
 
-  let selectedSeatTheme = {
+  //Seat theme css
+  const selectedSeatTheme = {
     bed: "border-2 border-[#007b2d] bg-[#65af80] cursor-pointer",
     pillow: "bg-[#007a2d]",
     text: "text-[#007a2d]",
   };
 
-  let availableSeatTheme = {
+  const availableSeatTheme = {
     bed: "border border-[#007b2d] cursor-pointer",
     pillow: "bg-[#e2eee3]",
     text: "text-[#1d1d1da3]",
   };
 
-  let availableForMaleOnly = {
+  const availableForMaleOnly = {
     bed: "border-2 border-[#587eee] bg-white cursor-pointer",
     pillow: "bg-[#e4ebf9]",
     text: "text-[#587eee]",
   };
 
-  let availableForFemaleOnly = {
+  const availableForFemaleOnly = {
     bed: "border-2 border-[#ff7ac9] bg-white cursor-pointer",
     pillow: "bg-[#ffe3f3]",
     text: "text-[#ff7ac9]",
   };
 
+  const booked = {
+    bed: "border-2 border-[#e8e8e8] bg-[#ebebeb]",
+    pillow: "bg-[#dfdfe4]",
+    text: "text-[#1d1d1da3]",
+  };
+
+  const bookedByMale = {
+    bed: "border-2 border-[#e4e4ef] bg-[#ebebeb]",
+    pillow: "bg-[#6a8df0]/10",
+    text: "text-[#1d1d1da3]",
+  };
+
+  const bookedByFemale = {
+    bed: "border-2 border-[#e4e4ef] bg-[#ebebeb]",
+    pillow: "bg-[#ff7ac9]/10",
+    text: "text-[#1d1d1da3]",
+  };
+
+  //change seat theme based on its status
   const getSeatTheme = (sTheme: SeatStatus): SeatTheme => {
     switch (sTheme) {
       case "available":
@@ -64,19 +90,41 @@ export const SleeperSeat = ({
         return availableForMaleOnly;
       case "availableForFemaleOnly":
         return availableForFemaleOnly;
+      case "booked":
+        return booked;
+      case "bookedByMale":
+        return bookedByMale;
+      case "bookedByFemale":
+        return bookedByFemale;
       default:
         return availableSeatTheme;
     }
   };
 
+  //function to handle click on seat
   const handleSeatOnClick = () => {
+    console.log("handleSeatOnClick called");
     switch (seatStatus) {
       case "available": {
         setSeatStatus("selected");
         break;
       }
       case "selected": {
-        setSeatStatus("available");
+        if (status === "availableForMaleOnly") {
+          setSeatStatus("availableForMaleOnly");
+        } else if (status === "availableForFemaleOnly") {
+          setSeatStatus("availableForFemaleOnly");
+        } else {
+          setSeatStatus("available");
+        }
+        break;
+      }
+      case "availableForMaleOnly": {
+        setSeatStatus("selected");
+        break;
+      }
+      case "availableForFemaleOnly": {
+        setSeatStatus("selected");
         break;
       }
     }
@@ -87,6 +135,7 @@ export const SleeperSeat = ({
       <button
         id={seatId}
         type="button"
+        disabled={isSeatBooked}
         className={`relative w-8 h-[70px] px-1 py-2 rounded-md flex justify-center items-end ${
           getSeatTheme(seatStatus).bed
         }`}
@@ -94,12 +143,24 @@ export const SleeperSeat = ({
       >
         {(seatStatus === "availableForMaleOnly" ||
           seatStatus === "bookedByMale") && (
-          <FaMale className="absolute top-1/2 left-1/2 -translate-y-1/2 text-[#6a8df0] -translate-x-1/2" />
+          <FaMale
+            className={`absolute top-1/2 left-1/2 -translate-y-1/2 ${
+              seatStatus === "availableForMaleOnly"
+                ? "text-[#6a8df0]"
+                : "text-[#6a8df0]/50"
+            } -translate-x-1/2`}
+          />
         )}
 
         {(seatStatus === "availableForFemaleOnly" ||
           seatStatus === "bookedByFemale") && (
-          <FaFemale className="absolute top-1/2 left-1/2 -translate-y-1/2 text-[#ff7ac9] -translate-x-1/2" />
+          <FaFemale
+            className={`absolute top-1/2 left-1/2 -translate-y-1/2 ${
+              seatStatus === "availableForFemaleOnly"
+                ? "text-[#ff7ac9]"
+                : "text-[#ff7ac9]/50"
+            } -translate-x-1/2`}
+          />
         )}
 
         <p
@@ -123,12 +184,12 @@ const SleeperBus = () => {
   }[] = [
     {
       seatId: "sleeper-lower-1",
-      status: "available",
+      status: "bookedByFemale",
       text: "₹500",
     },
     {
       seatId: "sleeper-lower-2",
-      status: "available",
+      status: "bookedByMale",
       text: "₹500",
     },
     {
@@ -148,7 +209,7 @@ const SleeperBus = () => {
     },
     {
       seatId: "sleeper-lower-6",
-      status: "available",
+      status: "bookedByMale",
       text: "₹500",
     },
   ];
@@ -173,7 +234,7 @@ const SleeperBus = () => {
     [
       {
         seatId: "sleeper-lower-3",
-        status: "available",
+        status: "bookedByMale",
         text: "₹500",
       },
       {
@@ -185,7 +246,7 @@ const SleeperBus = () => {
     [
       {
         seatId: "sleeper-lower-5",
-        status: "available",
+        status: "bookedByMale",
         text: "₹500",
       },
       {
@@ -197,7 +258,7 @@ const SleeperBus = () => {
     [
       {
         seatId: "sleeper-lower-7",
-        status: "available",
+        status: "bookedByMale",
         text: "₹500",
       },
       {
@@ -209,24 +270,24 @@ const SleeperBus = () => {
     [
       {
         seatId: "sleeper-lower-9",
-        status: "available",
+        status: "bookedByFemale",
         text: "₹500",
       },
       {
         seatId: "sleeper-lower-10",
-        status: "available",
+        status: "booked",
         text: "₹500",
       },
     ],
     [
       {
         seatId: "sleeper-lower-11",
-        status: "available",
+        status: "bookedByFemale",
         text: "₹500",
       },
       {
         seatId: "sleeper-lower-12",
-        status: "available",
+        status: "booked",
         text: "₹500",
       },
     ],
