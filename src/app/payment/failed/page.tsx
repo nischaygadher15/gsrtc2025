6d0d42d2ff1fbdd41f05abf90440f2843f4f90e2
nischaygadher15 <nischaygadher15@gmail.com>
2025-11-19 21:00:43 +0530

    loader test

--- src/app/payment/failed/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PaymentFailed = () => {
-  return <div></div>;
-};
-
-export default PaymentFailed;
--- src/app/payment/success/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PaymentSuccess = () => {
-  return <div>Payment successfully done.</div>;
-};
-
-export default PaymentSuccess;
