11f8ce9326fb197b156792c0f9a8589eaf559754
nischaygadher15 <nischaygadher15@gmail.com>
2025-12-14 18:29:07 +0530

    booking page done

--- src/app/bookings/Bookings.tsx
+++ src/app/bookings/Bookings.tsx
@@ -5,187 +5,93 @@ import Tab from "@mui/material/Tab";
 import { useState } from "react";
 import BookingCard from "@/components/common/BookingCard";
 import useWindowSize from "@/Hooks/useWindowSize";
-import defaultUser from "@/assets/images/default-user.svg";
-import Image from "next/image";
-import { IoSettingsOutline, IoTicketOutline } from "react-icons/io5";
+import Link from "next/link";
 
 const Bookings = () => {
-  const [mainTab, setMainTab] = useState<number>(0);
   const [bookingTab, setBookingTab] = useState<number>(0);
   let winSize = useWindowSize();
-
-  const handleMainTabs = (event: React.SyntheticEvent, newValue: number) => {
-    if (newValue <= 2) setMainTab(newValue);
-  };
+  const trips = [1, 2, 3, 4, 5];
 
   const handleBookingTbs = (event: React.SyntheticEvent, newValue: number) => {
     if (newValue <= 2) setBookingTab(newValue);
   };
 
   return (
-    <div className="bg-[#f2f2f8]">
-      <div className="flex min-h-[calc(100vh-84px)] items-stretch border-b border-b-slate-300">
-        <div className="w-[240px] border-e border-e-[#d1d5dc] ps-4 py-7">
-          {/* User photo */}
-          <div className="flex items-center gap-4 mb-7">
-            <Image
-              src={defaultUser}
-              alt="User profile image"
-              width={38}
-              height={38}
-              className=""
-            />
-            <p className="font-semibold">user@gmail.com</p>
-          </div>
-
-          {/* Tabs */}
-          <div>
+    <div className="myContainer bg-[#f2f2f8]">
+      <div className="md:px-10 lg:px-24">
+        <p className="font-semibold text-xl py-7">My Trips</p>
+        <div className="pb-7">
+          <div className="mb-7">
             <Tabs
-              orientation="vertical"
-              variant="scrollable"
-              value={mainTab}
-              onChange={handleMainTabs}
-              aria-label="Vertical tabs example"
+              value={bookingTab}
+              onChange={handleBookingTbs}
+              variant={winSize > 640 ? "standard" : "scrollable"}
+              scrollButtons={"auto"}
               sx={{
+                "& .MuiTab-root": {
+                  textAlign: "left",
+                  fontSize: 18,
+                  textTransform: "capitalize",
+                  fontWeight: 500,
+                },
+                "& .MuiTabs-indicator": {
+                  backgroundColor: "#173c62",
+                },
                 "& .Mui-selected": {
                   color: "#173c62 !important",
                 },
-                "& .MuiTab-root": {
-                  padding: "24px 16px",
-                  fontWeight: 600,
-                  textTransform: "none",
-                  alignItems: "flex-start",
-                  borderBottom: "1px solid #d1d5dc",
+                "& .MuiTabs-list": {
+                  gap: winSize > 640 ? "30px" : "20px",
                 },
               }}
             >
-              <Tab
-                label={
-                  <div className="flex items-center gap-4">
-                    <IoTicketOutline className="rotate-y-180 text-[22px]" />
-                    <span className="">My Trips</span>
-                  </div>
-                }
-              />
-              <Tab
-                label={
-                  <div className="flex items-center gap-4">
-                    <IoSettingsOutline className="rotate-y-180 text-[22px]" />
-                    <span className="">My Profile</span>
-                  </div>
-                }
-              />
+              <Tab label="Upcoming" />
+              <Tab label="Cancelled" />
+              <Tab label="Completed" />
             </Tabs>
           </div>
-        </div>
 
-        {/* Tabs Content */}
-        <div className="flex-1 px-24">
-          {/* My Trip */}
-          {mainTab === 0 && (
-            <div>
-              <p className="font-semibold text-xl py-7">My Trips</p>
-              <div className="pb-7">
-                <div className="mb-7">
-                  <Tabs
-                    value={bookingTab}
-                    onChange={handleBookingTbs}
-                    aria-label="basic tabs example"
-                    variant={winSize > 640 ? "standard" : "fullWidth"}
-                    sx={{
-                      "& .MuiTab-root": {
-                        // marginRight: winSize > 640 ? "100px" : "",
-                        textAlign: "left",
-                        // padding: 0,
-                        fontSize: 20,
-                        textTransform: "capitalize",
-                        fontWeight: 500,
-                      },
-                      "& .MuiTabs-indicator": {
-                        backgroundColor: "#173c62",
-                      },
-                      "& .Mui-selected": {
-                        color: "#173c62 !important",
-                      },
-                      "& .MuiTabs-list": {
-                        gap: "30px",
-                      },
-                    }}
-                  >
-                    <Tab label="Upcoming" />
-                    <Tab label="Cancelled" />
-                    <Tab label="Completed" />
-                  </Tabs>
-                </div>
-
-                {/* Upcoming */}
-                {bookingTab == 0 && (
-                  <ul className="flex flex-col gap-3">
-                    <li>
+          {/* Upcoming */}
+          {bookingTab == 0 && (
+            <ul className="flex flex-col gap-4">
+              {trips &&
+                trips.map((trip) => (
+                  <li key={`upcomming-${trip}`}>
+                    <Link href={`/ticket/view/${trip}`}>
                       <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                  </ul>
-                )}
-
-                {/* Cancelled */}
-                {bookingTab == 1 && (
-                  <ul className="flex flex-col gap-3">
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                  </ul>
-                )}
-
-                {/* Completed */}
-                {bookingTab == 2 && (
-                  <ul className="flex flex-col gap-3">
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                    <li>
-                      <BookingCard />
-                    </li>
-                  </ul>
-                )}
-              </div>
-            </div>
+                    </Link>
+                  </li>
+                ))}
+            </ul>
           )}
 
-          {/* My Profile */}
-          {mainTab === 1 && <div></div>}
+          {/* Cancelled */}
+          {bookingTab == 1 && (
+            <ul className="flex flex-col gap-4">
+              {trips &&
+                trips.map((trip) => (
+                  <li key={`cancelled-${trip}`}>
+                    <Link href={`/ticket/view/${trip}`}>
+                      <BookingCard />
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          )}
+
+          {/* Completed */}
+          {bookingTab == 2 && (
+            <ul className="flex flex-col gap-4">
+              {trips &&
+                trips.map((trip) => (
+                  <li key={`completed-${trip}`}>
+                    <Link href={`/ticket/view/${trip}`}>
+                      <BookingCard />
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
--- src/components/common/BookingCard.tsx
+++ src/components/common/BookingCard.tsx
@@ -1,9 +1,7 @@
 import Popover from "@mui/material/Popover";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import { FaEllipsisVertical } from "react-icons/fa6";
 import { useState } from "react";
+import { Menu, MenuItem } from "@mui/material";
 
 const BookingCard = () => {
   const [dotsAnchor, setDotsAnchor] = useState<HTMLButtonElement | null>(null);
@@ -17,248 +15,87 @@ const BookingCard = () => {
   };
   return (
     <div className="relative">
-      <Accordion
-        sx={{
-          "&.MuiAccordion-root": {
-            boxShadow: "0 0 10px rgba(0,0,0,0.3)",
-          },
-          "& .Mui-expanded": {
-            margin: 0,
-          },
-        }}
-      >
-        <AccordionSummary
-          // expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="confirmed-ticket-accordian"
-          className="!p-4 !m-0 bg-white"
-        >
-          <div className="w-full h-full">
-            <div className="pe-8 sm:pe-12 flex flex-col md:flex-row gap-5">
-              <div className="w-full md:w-1/2 flex justify-between gap-5">
-                <div className="w-[40%]">
-                  <div className=" inline-flex flex-col gap-1">
-                    <p className="text-4xl text-primary text-center font-semibold leading-none">
-                      20
-                    </p>
-                    <p className="sm:text-nowrap">December 2025</p>
-                    <p className="font-semibold text-center">Friday</p>
-                  </div>
-                </div>
-
-                <div className="flex flex-col gap-1 w-[60%]">
-                  <p className="font-semibold text-xl text-right md:text-left">
-                    Ahmedabad - Surat
-                  </p>
-                  <p className="font-medium text-sm text-right md:text-left">
-                    GSRTC
-                  </p>
-                </div>
-              </div>
-
-              <div className="w-full md:w-1/2 flex justify-between gap-5">
-                <div className="w-[60%]">
-                  <p className="font-semibold text-sm text-left">Boarding</p>
-                  <p className="font-medium text-sm text-left text-[#1d1d1da3]">
-                    Ahmedabad
-                  </p>
-                </div>
-
-                <div className="flex flex-col items-end w-[40%]">
-                  <p className="font-semibold text-sm text-left">Completed</p>
-                  <p className="sm:text-nowrap text-sm font-medium text-right text-[#1d1d1da3]">
-                    PNR No: {"XXXXXXXXX"}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </AccordionSummary>
-
-        <AccordionDetails className="">
-          <div className="sm:px-4 md:px-7 lg:px-15 border-t border-t-slate-200">
-            {/* Travel details */}
-            <div className="py-7 ">
-              <p className="mb-4 font-medium text-center text-[#1d1d1da3]">
-                Travel Details
-              </p>
-
-              <div className="flex justify-between items-center">
-                <div>
-                  <p className="font-semibold">GSRTC</p>
-                  <p className="text-[#1d1d1da3] text-sm font-medium">
-                    Non AC Seater (2+1)
-                  </p>
-                </div>
-                <div>
-                  <p className="font-semibold">Depature Time</p>
-                  <p className="font-medium text-sm text-[#1d1d1da3]">
-                    04:00 PM
-                  </p>
-                </div>
+      <div className="bg-white p-4 rounded-md shadow-[0_0_7px_rgba(0,0,0,0.3)]">
+        <div className="pe-6 sm:pe-12 flex flex-col md:flex-row gap-5">
+          <div className="w-full md:w-1/2 flex justify-between sm:gap-7 lg:gap-5">
+            <div className="w-[40%]">
+              <div className=" inline-flex flex-col gap-1">
+                <p className="text-4xl text-primary md:text-center font-semibold leading-none">
+                  20
+                </p>
+                <p className="sm:text-nowrap">December 2025</p>
+                <p className="hidden md:block font-semibold text-center">
+                  Friday
+                </p>
               </div>
             </div>
 
-            {/* Passenger details */}
-            <div className="py-7 border-t border-t-slate-200">
-              <p className="font-medium text-center text-[#1d1d1da3]">
-                Passenger Details
+            <div className="flex flex-col gap-1 w-[60%]">
+              <p className="font-semibold text-xl text-right md:text-left">
+                Ahmedabad - Surat
               </p>
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th className="p-1 text-sm font-semibold text-left">
-                      Seat
-                    </th>
-                    <th className="p-1 text-sm font-semibold text-left">
-                      Name
-                    </th>
-                    <th className="p-1 text-sm font-semibold text-center">
-                      Age
-                    </th>
-                    <th className="p-1 text-sm font-semibold text-center">
-                      Gender
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="p-1 font-semibold text-left">11</td>
-                    <td className="p-1 font-semibold text-left">
-                      NISCHAY K GADHER
-                    </td>
-                    <td className="p-1 font-semibold text-center">30</td>
-                    <td className="p-1 font-semibold text-center">MALE</td>
-                  </tr>
-                  <tr>
-                    <td className="p-1 font-semibold text-left">12</td>
-                    <td className="p-1 font-semibold text-left">
-                      MAHADEV AHIR
-                    </td>
-                    <td className="p-1 font-semibold text-center">26</td>
-                    <td className="p-1 font-semibold text-center">MALE</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
-            {/* Fare details */}
-            <div className="border-t border-t-slate-200 pt-7 ">
-              <p className="font-medium text-center text-[#1d1d1da3]">
-                Payment Details
+              <p className="font-medium text-sm text-right md:text-left">
+                GSRTC
               </p>
-              <table className="w-full">
-                <tbody>
-                  <tr>
-                    <td className="w-[80%]">
-                      <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                        Base Fare
-                      </p>
-                    </td>
-                    <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                      &#8377;620
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="w-[80%]">
-                      <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                        Free Cancellation
-                      </p>
-                    </td>
-                    <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                      &#8377;49
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="w-[80%]">
-                      <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                        Travel Insurance
-                      </p>
-                    </td>
-                    <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                      &#8377;30
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="w-[80%] pt-2 pb-4">
-                      <p className="text-sm text-[#1d1d1da3] font-medium">
-                        GST
-                      </p>
-                    </td>
-                    <td className="w-[20%] pt-2 pb-4 text-sm text-[#1d1d1da3] font-medium text-right">
-                      &#8377;30
-                    </td>
-                  </tr>
-                  <tr className="">
-                    <td className="w-[80%]">
-                      <p className="font-medium">Total Amount Paid</p>
-                    </td>
-                    <td className="w-[20%] font-bold text-right">
-                      &#8377;729.00
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
           </div>
-        </AccordionDetails>
-      </Accordion>
 
-      <div className="z-10 absolute top-4 right-0 sm:right-4">
-        <button
-          type="button"
-          className="p-3 text-[#1d1d1da3] hover:text-black cursor-pointer"
-          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-            e.stopPropagation();
-            openDotMenu(e);
-          }}
-        >
-          <FaEllipsisVertical className="text-2xl" />
-        </button>
-        <Popover
-          disableScrollLock
-          open={Boolean(dotsAnchor)}
-          anchorEl={dotsAnchor}
-          onClose={closeDotMenu}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-        >
-          <div className="flex flex-col">
-            <button
-              type="button"
-              className="p-3 cursor-pointer hover:bg-slate-200"
-              onClick={() => {
-                closeDotMenu();
-              }}
-            >
-              Rate and Review
-            </button>
-            <button
-              type="button"
-              className="p-3 cursor-pointer hover:bg-slate-200"
-              onClick={() => {
-                closeDotMenu();
-              }}
-            >
-              Email Ticket
-            </button>
-            <button
-              type="button"
-              className="p-3 cursor-pointer hover:bg-slate-200"
-              onClick={() => {
-                closeDotMenu();
-              }}
-            >
-              Show Ticket
-            </button>
+          <div className="w-full md:w-1/2 flex justify-between gap-5">
+            <div className="w-[40%]">
+              <p className="font-semibold text-sm text-left">Boarding</p>
+              <p className="font-medium text-sm text-left text-[#1d1d1da3]">
+                Ahmedabad
+              </p>
+            </div>
+
+            <div className="flex flex-col items-end md:items-start w-[60%]">
+              <p className="font-semibold text-sm text-left">Completed</p>
+              <p className="flex flex-col sm:flex-row text-sm font-medium text-right text-[#1d1d1da3]">
+                <span>PNR No:</span>
+                <span>{"XXXXXXXXX"}</span>
+              </p>
+              <p className="flex flex-col sm:flex-row text-sm font-medium text-right text-[#1d1d1da3]">
+                <span>Ref. No:</span>
+                <span>{"XXXXXXXXX"}</span>
+              </p>
+            </div>
           </div>
-        </Popover>
+        </div>
+      </div>
+
+      <div className="z-10 absolute top-3 right-0 sm:right-4">
+        <div>
+          <button
+            type="button"
+            aria-controls={Boolean(dotsAnchor) ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={Boolean(dotsAnchor) ? "true" : undefined}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              e.stopPropagation();
+              openDotMenu(e);
+            }}
+            className="p-1.5 text-[#1d1d1da3] hover:text-black cursor-pointer"
+          >
+            <FaEllipsisVertical className="text-2xl" />
+          </button>
+          <Menu
+            anchorEl={dotsAnchor}
+            open={Boolean(dotsAnchor)}
+            onClose={closeDotMenu}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem onClick={closeDotMenu}>Rate and Review</MenuItem>
+            <MenuItem onClick={closeDotMenu}>Email Ticket</MenuItem>
+            <MenuItem onClick={closeDotMenu}>Show Ticket</MenuItem>
+          </Menu>
+        </div>
       </div>
     </div>
   );
