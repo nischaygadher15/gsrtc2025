edbfdc0ca7715ccb045f8bd3e013b69d208b0228
nischaygadher15 <nischaygadher15@gmail.com>
2025-12-15 13:27:14 +0530

    working on ticket page

--- src/app/ticket/view/[ticketId]/Ticket.tsx
+++ src/app/ticket/view/[ticketId]/Ticket.tsx
@@ -21,6 +21,8 @@ import Image from "next/image";
 import gsrtcAppQR from "@/assets/images/GSRTC_App_QR.svg";
 import gStore from "@/assets/images/playstore.svg";
 import appStore from "@/assets/images/appstore.svg";
+import { LuDot } from "react-icons/lu";
+import { MdChatBubbleOutline } from "react-icons/md";
 
 const ViewTicket = () => {
   const router = useRouter();
@@ -52,156 +54,236 @@ const ViewTicket = () => {
 
   return (
     <div className="w-full h-full myContainer bg-[#f2f2f8] py-10 flex flex-col lg:flex-row gap-7">
-      <div
-        className="w-full lg:w-3/4 p-4 bg-white rounded-sm"
-        style={{
-          boxShadow: "0 0 10px rgba(0,0,0,0.3)",
-        }}
-      >
-        <div className="flex justify-between mb-7">
-          <div className="flex flex-col sm:flex-row items-start gap-5">
-            <div className="flex flex-col gap-1">
-              <p className="text-5xl text-primary text-center leading-none">
-                20
-              </p>
-              <p className="text-[#1d1d1da3] text-center">Nov 2025</p>
-              <p className="text-xs font-semibold px-1.5 py-1 uppercase bg-green-500 text-white text-semibold">
-                Confirmed
-              </p>
+      <div className="w-full lg:w-3/4 flex flex-col gap-y-5">
+        {/* Ticket & Fairs Details */}
+        <div
+          className="p-4 bg-white rounded-sm"
+          style={{
+            boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+          }}
+        >
+          <div className="flex justify-between mb-7">
+            <div className="flex flex-col sm:flex-row items-start gap-5">
+              <div className="flex flex-col gap-1">
+                <p className="text-5xl text-primary text-center leading-none">
+                  20
+                </p>
+                <p className="text-[#1d1d1da3] text-center">Nov 2025</p>
+                <p className="text-xs font-semibold px-1.5 py-1 uppercase bg-green-500 text-white text-semibold">
+                  Confirmed
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <p className="font-semibold">AHMEDABAD - SURAT</p>
+                <p className="font-semibold text-sm">GSRTC</p>
+                <p className="text-[#1d1d1da3] text-sm">Non AC Seater (2+1)</p>
+              </div>
             </div>
+
             <div className="flex flex-col gap-1">
-              <p className="font-semibold">AHMEDABAD - SURAT</p>
-              <p className="font-semibold text-sm">GSRTC</p>
-              <p className="text-[#1d1d1da3] text-sm">Non AC Seater (2+1)</p>
+              <a
+                href="/sample.pdf"
+                className="text-right cursor-pointer text-blue-500"
+                download
+                onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+                  e.stopPropagation();
+                }}
+              >
+                Print/Download
+              </a>
+              <p className="font-semibold text-right">PNR No: XXXXXXXXX</p>
+              <p className="font-semibold text-right">Trip Code: XXXXXXXXX</p>
+              <p className="font-semibold text-right">Route No: XXXXXXXXX</p>
             </div>
           </div>
 
-          <div className="flex flex-col gap-1">
-            <a
-              href="/sample.pdf"
-              className="text-right cursor-pointer text-blue-500"
-              download
-              onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
-                e.stopPropagation();
-              }}
-            >
-              Print/Download
-            </a>
-            <p className="font-semibold text-right">PNR No: XXXXXXXXX</p>
-            <p className="font-semibold text-right">Trip Code: XXXXXXXXX</p>
-            <p className="font-semibold text-right">Route No: XXXXXXXXX</p>
+          <div className="grid grid-cols-2 sm:grid-cols-[3fr_2fr_2fr_3fr] grid-rows-2 sm:grid-rows-1">
+            <div className="order-1">
+              <p className=" text-left p-1 text-sm text-[#1d1d1da3]">
+                BOARDING
+              </p>
+              <p className="p-1 font-semibold text-left">AHMEDABAD</p>
+            </div>
+            <div className="order-3 sm:order-1">
+              <p className="text-left sm:text-center p-1 text-sm text-[#1d1d1da3]">
+                DEPATURE
+              </p>
+              <p className="p-1 font-semibold text-left sm:text-center">
+                00:40
+              </p>
+            </div>
+            <div className="order-4 sm:order-3">
+              <p className="text-right sm:text-center p-1 text-sm text-[#1d1d1da3]">
+                PASSENGERS
+              </p>
+              <p className="p-1 font-semibold text-right sm:text-center">2</p>
+            </div>
+            <div className="order-2 sm:order-4">
+              <p className="text-right p-1 text-sm text-[#1d1d1da3]">
+                DROP OFF
+              </p>
+              <p className="p-1 font-semibold text-right">SURAT BUS STATION</p>
+            </div>
+          </div>
+
+          {/* Passenger details */}
+          <div className="py-7 border-t border-t-slate-200">
+            <p className="font-bold text-center text-[#1d1d1da3]">
+              PASSENGER DETAILS
+            </p>
+            <table className="w-full">
+              <thead>
+                <tr>
+                  <th className="p-1 text-sm font-semibold text-left">Seat</th>
+                  <th className="p-1 text-sm font-semibold text-left">Name</th>
+                  <th className="p-1 text-sm font-semibold text-center">Age</th>
+                  <th className="p-1 text-sm font-semibold text-center">
+                    Gender
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="p-1 font-semibold text-left">11</td>
+                  <td className="p-1 font-semibold text-left">
+                    NISCHAY K GADHER
+                  </td>
+                  <td className="p-1 font-semibold text-center">30</td>
+                  <td className="p-1 font-semibold text-center">MALE</td>
+                </tr>
+                <tr>
+                  <td className="p-1 font-semibold text-left">12</td>
+                  <td className="p-1 font-semibold text-left">MAHADEV AHIR</td>
+                  <td className="p-1 font-semibold text-center">26</td>
+                  <td className="p-1 font-semibold text-center">MALE</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          {/* Fare details */}
+          <div className="border-t border-t-slate-200 pt-7 ">
+            <p className="font-bold text-center text-[#1d1d1da3]">
+              Fare Details
+            </p>
+            <table className="w-full">
+              <tbody>
+                <tr>
+                  <td className="w-[80%]">
+                    <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
+                      Base Fare
+                    </p>
+                  </td>
+                  <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
+                    &#8377;620
+                  </td>
+                </tr>
+                <tr>
+                  <td className="w-[80%]">
+                    <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
+                      Free Cancellation
+                    </p>
+                  </td>
+                  <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
+                    &#8377;49
+                  </td>
+                </tr>
+                <tr>
+                  <td className="w-[80%]">
+                    <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
+                      Travel Insurance
+                    </p>
+                  </td>
+                  <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
+                    &#8377;30
+                  </td>
+                </tr>
+                <tr>
+                  <td className="w-[80%] pt-2 pb-4">
+                    <p className="text-sm text-[#1d1d1da3] font-medium">GST</p>
+                  </td>
+                  <td className="w-[20%] pt-2 pb-4 text-sm text-[#1d1d1da3] font-medium text-right">
+                    &#8377;30
+                  </td>
+                </tr>
+                <tr className="">
+                  <td className="w-[80%]">
+                    <p className="font-medium">Total Amount Paid</p>
+                  </td>
+                  <td className="w-[20%] font-bold text-right">
+                    &#8377;729.00
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-[3fr_2fr_2fr_3fr] grid-rows-2 sm:grid-rows-1">
-          <div className="order-1">
-            <p className=" text-left p-1 text-sm text-[#1d1d1da3]">BOARDING</p>
-            <p className="p-1 font-semibold text-left">AHMEDABAD</p>
-          </div>
-          <div className="order-3 sm:order-1">
-            <p className="text-left sm:text-center p-1 text-sm text-[#1d1d1da3]">
-              DEPATURE
-            </p>
-            <p className="p-1 font-semibold text-left sm:text-center">00:40</p>
-          </div>
-          <div className="order-4 sm:order-3">
-            <p className="text-right sm:text-center p-1 text-sm text-[#1d1d1da3]">
-              PASSENGERS
-            </p>
-            <p className="p-1 font-semibold text-right sm:text-center">2</p>
-          </div>
-          <div className="order-2 sm:order-4">
-            <p className="text-right p-1 text-sm text-[#1d1d1da3]">DROP OFF</p>
-            <p className="p-1 font-semibold text-right">SURAT BUS STATION</p>
-          </div>
+        {/* Live Tracking */}
+        <div
+          className="p-4 bg-white rounded-sm"
+          style={{
+            boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+          }}
+        >
+          <p className="text-xl font-semibold mb-4">Live Tracking</p>
+
+          <table className="mx-auto border border-collapse">
+            <tbody>
+              <tr className="border">
+                <td className="border px-4 py-2 font-semibold">Route</td>
+                <td className="border px-4 py-2">Jamnagar - Ahmedabad</td>
+              </tr>
+              <tr>
+                <td className="border px-4 py-2 font-semibold">Bus No.</td>
+                <td className="border px-4 py-2">GJ 10 XX XXXX</td>
+              </tr>
+              <tr>
+                <td colSpan={2} className="border px-4 py-2 text-center">
+                  <button
+                    type="button"
+                    className="w-full sm:w-[240px] rounded-s-full rounded-e-full py-2 cursor-pointer bg-primary rounded text-center text-white"
+                  >
+                    Track your bus
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        {/* Passenger details */}
-        <div className="py-7 border-t border-t-slate-200">
-          <p className="font-bold text-center text-[#1d1d1da3]">
-            PASSENGER DETAILS
+        {/* Customer Care */}
+        <div
+          className="p-4 bg-white rounded-sm"
+          style={{
+            boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+          }}
+        >
+          <p className="text-xl font-semibold mb-4">Customer Care</p>
+
+          <p className="text-lg font-semibold text-center my-5">
+            Need any help?
           </p>
-          <table className="w-full">
-            <thead>
-              <tr>
-                <th className="p-1 text-sm font-semibold text-left">Seat</th>
-                <th className="p-1 text-sm font-semibold text-left">Name</th>
-                <th className="p-1 text-sm font-semibold text-center">Age</th>
-                <th className="p-1 text-sm font-semibold text-center">
-                  Gender
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="p-1 font-semibold text-left">11</td>
-                <td className="p-1 font-semibold text-left">
-                  NISCHAY K GADHER
-                </td>
-                <td className="p-1 font-semibold text-center">30</td>
-                <td className="p-1 font-semibold text-center">MALE</td>
-              </tr>
-              <tr>
-                <td className="p-1 font-semibold text-left">12</td>
-                <td className="p-1 font-semibold text-left">MAHADEV AHIR</td>
-                <td className="p-1 font-semibold text-center">26</td>
-                <td className="p-1 font-semibold text-center">MALE</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
 
-        {/* Fare details */}
-        <div className="border-t border-t-slate-200 pt-7 ">
-          <p className="font-bold text-center text-[#1d1d1da3]">Fare Details</p>
-          <table className="w-full">
-            <tbody>
-              <tr>
-                <td className="w-[80%]">
-                  <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                    Base Fare
-                  </p>
-                </td>
-                <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                  &#8377;620
-                </td>
-              </tr>
-              <tr>
-                <td className="w-[80%]">
-                  <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                    Free Cancellation
-                  </p>
-                </td>
-                <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                  &#8377;49
-                </td>
-              </tr>
-              <tr>
-                <td className="w-[80%]">
-                  <p className="pt-2 text-sm text-[#1d1d1da3] font-medium">
-                    Travel Insurance
-                  </p>
-                </td>
-                <td className="pt-2 w-[20%] text-sm text-[#1d1d1da3] font-medium text-right">
-                  &#8377;30
-                </td>
-              </tr>
-              <tr>
-                <td className="w-[80%] pt-2 pb-4">
-                  <p className="text-sm text-[#1d1d1da3] font-medium">GST</p>
-                </td>
-                <td className="w-[20%] pt-2 pb-4 text-sm text-[#1d1d1da3] font-medium text-right">
-                  &#8377;30
-                </td>
-              </tr>
-              <tr className="">
-                <td className="w-[80%]">
-                  <p className="font-medium">Total Amount Paid</p>
-                </td>
-                <td className="w-[20%] font-bold text-right">&#8377;729.00</td>
-              </tr>
-            </tbody>
-          </table>
+          <p className="text-sm flex justify-center items-center mb-4">
+            <span>24x7 support</span>
+            <span>
+              <LuDot />
+            </span>
+            <span>Quick resolution</span>
+            <span>
+              <LuDot />
+            </span>
+            <span>Multilingual</span>
+          </p>
+
+          <button
+            type="button"
+            className="w-full sm:w-[240px] mx-auto rounded-s-full rounded-e-full px-10 bg-primary text-center text-white flex justify-center items-center gap-4 py-2 cursor-pointer"
+          >
+            <MdChatBubbleOutline className="text-xl" />
+            <span>Chat with us</span>
+          </button>
         </div>
       </div>
 
@@ -369,10 +451,10 @@ const ViewTicket = () => {
           <table className="max-w-[400px]">
             <tbody>
               <tr>
-                <td className="w-1/2 text-xs text-[#1d1d1da3] px-2.5 py-1">
+                <td className="w-1/2 text-xs text-center text-[#1d1d1da3] px-2.5 py-1">
                   Scan to download
                 </td>
-                <td className="w-1/2 text-xs text-[#1d1d1da3] px-2.5 py-1">
+                <td className="w-1/2 text-xs text-center text-[#1d1d1da3] px-2.5 py-1">
                   Download the App on
                 </td>
               </tr>
